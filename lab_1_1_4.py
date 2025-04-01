fe20b514132a2fa2ff3a5e7496601dd2c9886df2
Полина Тырыкина <polinatyrykina@gmail.com>
2025-04-01 21:40:06 +0700

    Итоговые правки

--- lab_1_1_4.py
+++ lab_1_1_4.py
@@ -7,29 +7,10 @@
                     # Количество всех чисел последовательности (решить задачу используя
                     # циклическую конструкцию while)
 
-
-print("Считаем сумму. Введите числа через пробел:")
-input_str = input().strip() + ' '  # Добавляем пробел для обработки всех чисел, в том числе последнего! 
+print("Введите числа через пробел:")
+input_str = input().strip() + ' '  # Добавляем пробел для обработки всех чисел
 
 total_sum = 0
-i = 0
-num_str = ""
-
-while i < len(input_str):
-    char = input_str[i]
-    if char == ' ':
-        if num_str:  # Если строка числа не пуста
-            total_sum += int(num_str)
-            num_str = ""  # Сброс
-    else:
-        num_str += char  # Накапливаем цифры числа
-    i += 1
-
-print(f"Сумма всех чисел: {total_sum}")
-
-print("Считаем количество. Введите числа через пробел:")
-input_str = input().strip() + ' ' 
-
 count = 0
 i = 0
 num_str = ""
@@ -37,11 +18,13 @@ num_str = ""
 while i < len(input_str):
     char = input_str[i]
     if char == ' ':
-        if num_str:
+        if num_str:  
+            total_sum += int(num_str)
             count += 1
-            num_str = ""
+            num_str = ""  # Сброс
     else:
-        num_str += char 
+        num_str += char  # Накапливаем цифры числа
     i += 1
 
+print(f"Сумма всех чисел: {total_sum}")
 print(f"Количество всех чисел: {count}")
